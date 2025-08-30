dcfdfaa99c78d6c851a030467bc38d0e68b028bc
KAPILKASHYAP-99 <kapilkashyap.inwork@gmail.com>
2025-08-30 16:14:29 +0530

    netflix clone

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className="Footer">
+      <ul>
+        <li>Help Centre</li>
+        <li>Gift Cards</li>
+        <li>Media Centre</li>
+        <li>Terms of Use</li>
+        <li>Legal Notices</li>
+        <li>Contact Us</li>
+      </ul>
+ 
+    </div>
+
+  )
+}
+
+export default Footer
